2f29a7d2b0c55f47264f397a093b29d5b5873893
rishabhsinghvi <rdpsinghvi@gmail.com>
2020-07-17 13:55:09 -0400

    coalesced headers + body

---
+++ include/shapes/prism_shapes.h
@@ -0,0 +1,35 @@
+/*
+* MIT License
+*
+* Copyright (c) 2020 Rishabh Singhvi
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
+#ifndef PRISM_SHAPES_H
+#define PRISM_SHAPES_H
+
+#include "shapes/prism_base_shape.h"
+#include "shapes/prism_sphere.h"
+#include "shapes/prism_capsule.h"
+#include "shapes/prism_box.h"
+#include "shapes/prism_triangle.h"
+
+#endif
